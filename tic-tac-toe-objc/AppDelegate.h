bf15f29a7bd85cfaed1d6e710e86f0f8c21553d0
Ben Stone <benjamin.h.stone@gmail.com>
2018-05-14 10:16:25 -0400

    Initial Commit

---
+++ tic-tac-toe-objc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tic-tac-toe-objc
+//
+//  Created by C4Q  on 5/14/18.
+//  Copyright © 2018 C4Q . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tic-tac-toe-objc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tic-tac-toe-objc
+//
+//  Created by C4Q  on 5/14/18.
+//  Copyright © 2018 C4Q . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
